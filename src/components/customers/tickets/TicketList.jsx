0c6cb890c4d45c3e6d53e70d4844d876f8e72ce4
Taylor Albright <jtayloralbright@gmail.com>
2024-09-05 15:04:53 -0500

    finished testing some stuff

--- src/components/customers/tickets/TicketList.jsx
+++ src/components/customers/tickets/TicketList.jsx
@@ -1,44 +1,21 @@
-import { useEffect, useState } from "react"
-import { getAllTickets } from "../../../services/ticketService"
-import { Ticket } from "./Ticket"
-import "./Tickets.css"
+import { useState, useEffect } from "react"
+import { getEmployees } from "../../../services/employeeService"
 
 export const TicketList = () => {
-    const [allTickets, setAllTickets] = useState([]) // [stateVariable, setterFunction]
-    const [showEmergencyOnly, setShowEmergencyOnly] = useState(false)
-    const [filteredTickets, setFilteredTickets] = useState([])
+  const [state, setState] = useState([])
 
-    useEffect(() => {
-      getAllTickets().then(ticketsArray => {
-        setAllTickets(ticketsArray)
-      })
-    }, [])
+  useEffect(() => {
+    getEmployees().then(employees => {
+      setState(employees)
+    })
+  }, [])
 
-    //Function is what we want to happen, the array is when we want it to happen
-    //useEffect will only run on the initial render when the dependency array is empty -> []
-    
-    useEffect(() => {
-      if (showEmergencyOnly) {
-        const emergencyTickets = allTickets.filter(ticket => ticket.emergency)
-        setFilteredTickets(emergencyTickets)
-      } else {
-        setFilteredTickets(allTickets)
-      }
-    }, [showEmergencyOnly, allTickets])
-
-    return (
-    <div className='tickets-container'>
-      <h2>Tickets</h2>
-      <div>
-
-      </div>
-      <article className='tickets'>
-        {filteredTickets.map(ticketObj => {
-          return (
-            <Ticket ticket={ticketObj} key={ticketObj.id}/>
-          )
-        })}
-      </article>
-    </div>
+  return (
+    <section>
+      {state.map(employee => {
+        console.log(employee)
+        return <div>{employee.user.fullName}</div>
+      })}
+    </section>
   )
 }
--- src/services/ticketService.jsx
+++
@@ -1,3 +0,0 @@
-export const getAllTickets = async () => {
-    return fetch(`http://localhost:8088/serviceTickets?_embed=employeeTickets`).then(res => res.json())
-}
